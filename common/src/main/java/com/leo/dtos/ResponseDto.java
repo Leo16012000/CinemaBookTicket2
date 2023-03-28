f90cc630223b0884cfb52df34030d5d33decaea2
hoquangnam45 <hoquangnam45@gmail.com>
2023-03-28 10:19:06 +0700

    add common util

---
+++ common/src/main/java/com/leo/dtos/ResponseDto.java
@@ -0,0 +1,18 @@
+package com.leo.dtos;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class ResponseDto<T> {
+  private String message;
+  @JacksonXmlProperty(localName = "payload")
+  private T payload;
+  private String serviceName;
+  private String id;
+}
---
+++ common/src/main/java/com/leo/utils/TimeHandle.java
@@ -0,0 +1,17 @@
+package com.leo.utils;
+
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class TimeHandle {
+  public static Timestamp dateStimestampConversion(String dateString) throws ParseException {
+    SimpleDateFormat dateFormat = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy");
+    dateFormat.setTimeZone(TimeZone.getTimeZone("ICT"));
+    Date date = dateFormat.parse(dateString);
+    Timestamp timestamp = new Timestamp(date.getTime());
+    return timestamp;
+  }
+}
